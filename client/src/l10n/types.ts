736e2ff9f5aa1eedf0816fd4e515934e852f6143
Hendrik Volschenk <righteous.trespasser@gmail.com>
2023-03-17 23:45:07 +0100

    feat(client): style authentication page

--- client/src/l10n/types.ts
+++ client/src/l10n/types.ts
@@ -1,7 +1,12 @@
 export interface L10n {
   actionTryAgain: string;
+  applicationName: string;
+  authenticateActionLogin: string;
+  authenticateAlertNoRegistrationRequired: string;
   authenticateGoogleActionLogin: string;
   authenticateGoogleErrorLoginFailed: string;
   authenticateProviderErrorLoading: string;
+  authentication: string;
+  home: string;
   temp: string;
 }
--- client/src/pages/Authentication/index.tsx
+++ client/src/pages/Authentication/index.tsx
@@ -1,8 +1,15 @@
+import Alert from '@mui/material/Alert';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 import FormHelperText from '@mui/material/FormHelperText';
 import React from 'react';
 
+import PageTitle from '~/src/components/PageTitle';
+import l10n from '~/src/l10n';
 import { useAuthentication } from '~/src/providers/Authentication';
 import { User } from '~/src/types/User';
+import { root } from '~/src/urls';
 
 import Google from './Providers/Google';
 import { AuthenticationProviderProps, AuthenticationProviders } from './types';
@@ -44,26 +51,44 @@ const Authentication: React.FC = () => {
 
   return (
     <React.Fragment>
-      {authenticatedUser && (
-        <p data-testid="authentication__user">{authenticatedUser.email}</p>
-      )}
-      {(Object.keys(authenticationProviders) as AuthenticationProviders[]).map(
-        (key) => {
-          const Provider = authenticationProviders[key];
-          return (
-            <Provider
-              isSubmitting={isSubmitting}
-              key={key}
-              onError={onError}
-              onStart={onStart}
-              onSuccess={onSuccess}
-            />
-          );
-        },
-      )}
-      <FormHelperText data-testid="authentication__error" error>
-        {error}
-      </FormHelperText>
+      <PageTitle
+        breadcrumbs={[
+          { title: l10n.home, url: root() },
+          { title: l10n.authentication },
+        ]}
+        title={l10n.authentication}
+      />
+      <Card>
+        <CardContent>
+          <Box marginBottom={2}>
+            <Alert severity="info">
+              {l10n.formatString(l10n.authenticateAlertNoRegistrationRequired, {
+                applicationName: l10n.applicationName,
+              })}
+            </Alert>
+          </Box>
+          {authenticatedUser && (
+            <p data-testid="authentication__user">{authenticatedUser.email}</p>
+          )}
+          {(
+            Object.keys(authenticationProviders) as AuthenticationProviders[]
+          ).map((key) => {
+            const Provider = authenticationProviders[key];
+            return (
+              <Provider
+                isSubmitting={isSubmitting}
+                key={key}
+                onError={onError}
+                onStart={onStart}
+                onSuccess={onSuccess}
+              />
+            );
+          })}
+          <FormHelperText data-testid="authentication__error" error>
+            {error}
+          </FormHelperText>
+        </CardContent>
+      </Card>
     </React.Fragment>
   );
 };
