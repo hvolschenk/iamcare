852689d99b524016339a595ef2372408c4bceffb
Hendrik Volschenk <righteous.trespasser@gmail.com>
2023-06-02 23:31:19 +0200

    fix(api): article seeder proper values

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ArticlesTableSeeder::class);
+        $this->call(ArticleSeeder::class);
         $this->call(CategorySeeder::class);
     }
 }
