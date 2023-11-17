2064b96f50912684cd3b515521d88fbf38743064
Hendrik Volschenk <righteous.trespasser@gmail.com>
2023-11-17 22:26:37 +0100

    chore(client): rewrite urls test to add more more easily

--- client/src/urls.test.ts
+++ client/src/urls.test.ts
@@ -1,47 +1,52 @@
-import { faker } from '@faker-js/faker';
-
 import {
   authentication,
   itemsSearch,
   root,
+  user,
+  userItems,
+  userItemsCreate,
   urlLayout,
   urlRelative,
 } from './urls';
 
-test('authentication', () => {
-  expect(authentication()).toBe('/authentication');
-});
+type TestCase = [
+  name: string,
+  method: Function,
+  parameters: any[],
+  result: string,
+];
 
-describe('itemsSearch', () => {
-  test('With no parameters', () => {
-    expect(itemsSearch()).toBe('/items/search?');
-  });
-
-  test('With all parameters', () => {
-    const distance = faker.number.int();
-    const location = faker.string.uuid();
-    const page = faker.number.int();
-    const perPage = faker.number.int();
-    const query = faker.word.sample();
-    const options = { distance, location, page, perPage, query };
-    expect(itemsSearch(options)).toBe(
-      `/items/search?distance=${distance.toString()}&location=${location}&page=${page.toString()}&perPage=${perPage.toString()}&query=${query}`,
-    );
-  });
-});
-
-test('root', () => {
-  expect(root()).toBe('/');
-});
-
-describe('urlLayout', () => {
-  test('Adds a * at the end of the URL', () => {
-    expect(urlLayout('/one/two')).toBe('/one/two/*');
-  });
-});
-
-describe('urlRelative', () => {
-  test('Removes the parent section from the URL', () => {
-    expect(urlRelative('/one/two', '/one')).toBe('/two');
-  });
+test.each<TestCase>([
+  ['authentication', authentication, [], '/authentication'],
+  ['itemsSearch (no paramaters)', itemsSearch, [], '/items/search?'],
+  [
+    'itemsSearch',
+    itemsSearch,
+    [
+      {
+        distance: 22,
+        location: 'Centurion',
+        page: 5,
+        perPage: 10,
+        query: 'Television',
+      },
+    ],
+    '/items/search?distance=22&location=Centurion&page=5&perPage=10&query=Television',
+  ],
+  ['root', root, [], '/'],
+  ['user (no paramaters)', user, [], '/users/:userID'],
+  ['user', user, ['22'], '/users/22'],
+  ['userItems (no paramaters)', userItems, [], '/users/:userID/items'],
+  ['userItems', userItems, ['22'], '/users/22/items'],
+  [
+    'userItemsCreate (no paramaters)',
+    userItemsCreate,
+    [],
+    '/users/:userID/items/create',
+  ],
+  ['userItemsCreate', userItemsCreate, ['22'], '/users/22/items/create'],
+  ['urlLayout', urlLayout, ['/one/two'], '/one/two/*'],
+  ['urlRelative', urlRelative, ['/one/two', '/one'], '/two'],
+])('Renders the %s URL', (name, method, parameters, result) => {
+  expect(method(...parameters)).toBe(result);
 });
