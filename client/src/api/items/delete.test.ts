20a929f484e64ac183b2c0854608b051af7172ae
Hendrik Volschenk <righteous.trespasser@gmail.com>
2023-11-30 23:06:28 +0100

    chore(client): fixup delete test copy paste mistakes

--- client/src/api/items/delete.test.ts
+++ client/src/api/items/delete.test.ts
@@ -4,10 +4,10 @@ import apiClient from '../client';
 jest.mock('../client');
 
 beforeEach(() => {
-  (apiClient.post as jest.Mock).mockClear();
+  (apiClient.delete as jest.Mock).mockClear();
   itemDelete('22');
 });
 
-test('Posts the correct values', () => {
+test('Sends the correct values', () => {
   expect(apiClient.delete).toHaveBeenCalledWith('/items/22');
 });
