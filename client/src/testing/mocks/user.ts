e73cf9190956b6104689ccb95bfc2b7329527ebf
Hendrik Volschenk <righteous.trespasser@gmail.com>
2024-01-17 22:47:42 +0100

    chore(client): more consistent user mock
    make the user name/surname match the email address

--- client/src/testing/mocks/user.ts
+++ client/src/testing/mocks/user.ts
@@ -5,14 +5,21 @@ import { faker } from '@faker-js/faker';
 
 import { User } from '~/src/types/User';
 
-const user = (partialUser?: Partial<User>): User => ({
-  avatar: faker.image.avatar(),
-  dateCreated: faker.date.past().toString(),
-  dateUpdated: faker.date.past().toString(),
-  email: faker.helpers.maybe(faker.internet.email),
-  id: faker.number.int(),
-  name: faker.person.fullName(),
-  ...partialUser,
-});
+const user = (partialUser?: Partial<User>): User => {
+  const firstName = faker.person.firstName();
+  const lastName = faker.person.lastName();
+
+  return {
+    avatar: faker.image.avatar(),
+    dateCreated: faker.date.past().toString(),
+    dateUpdated: faker.date.past().toString(),
+    email: faker.helpers.maybe(() =>
+      faker.internet.email({ firstName, lastName }),
+    ),
+    id: faker.number.int(),
+    name: faker.person.fullName({ firstName, lastName }),
+    ...partialUser,
+  };
+};
 
 export default user;
