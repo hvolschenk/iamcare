aa08325b43a1bb8c57ca158b0459406a250b6ebf
Hendrik Volschenk <righteous.trespasser@gmail.com>
2024-04-10 22:36:00 +0200

    fix(client): ensure x-xsrf-token header is always sent to api

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -6,6 +6,11 @@ import configuration from '../configuration';
 const api = axios.create({
   baseURL: configuration.api.baseURL(),
   withCredentials: true,
+  // This is theoretically considered unsafe.
+  // See: https://github.com/axios/axios/issues/6006
+  // In this case, however, the `baseURL` makes it impossible
+  // for any requests to escape and send the `X-XSRF-Token` to bad actors.
+  withXSRFToken: true,
 });
 
 api.interceptors.request.use((config) => {
