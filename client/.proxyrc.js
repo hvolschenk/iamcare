87153df839ffc90c3eb871b6fae471522c42b399
Hendrik Volschenk <righteous.trespasser@gmail.com>
2024-04-17 09:13:08 +0200

    feat(client): add content security policy

---
+++ client/.proxyrc.js
@@ -0,0 +1,43 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use((request, response, next) => {
+    if (!request.url.startsWith('/api')) {
+      const contentSecurityPolicy = [
+        ['default-src', "'self'"].join(' '),
+        ['connect-src', "'self'", 'https://maps.googleapis.com'].join(' '),
+        ['font-src', 'https://fonts.gstatic.com'].join(' '),
+        [
+          'img-src',
+          "'self'",
+          process.env.API_URL,
+          'https://*.googleusercontent.com',
+        ].join(' '),
+        [
+          'script-src',
+          "'self'",
+          'https://accounts.google.com',
+          'https://maps.googleapis.com',
+        ].join(' '),
+        [
+          'style-src',
+          "'self'",
+          "'unsafe-inline'",
+          'https://fonts.googleapis.com',
+        ].join(' '),
+      ].join('; ');
+      response.setHeader('Content-Security-Policy', contentSecurityPolicy);
+    }
+    next();
+  });
+
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://api:8000/',
+      pathRewrite: {
+        '^/api': '',
+      },
+    }),
+  );
+};
