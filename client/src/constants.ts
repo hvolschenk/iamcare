b38b1f0707a9c329b7447a75f3bb29582f38598c
Hendrik Volschenk <righteous.trespasser@gmail.com>
2024-06-01 23:02:46 +0200

    chore(client): add constant value for search location distance

---
+++ client/src/constants.ts
@@ -0,0 +1,5 @@
+// The default distance to use when filtering by location
+// and no distance is given.
+// 10 | 25 | 50 | 100
+// eslint-disable-next-line import/prefer-default-export
+export const SEARCH_DISTANCE_DEFAULT = 25;
