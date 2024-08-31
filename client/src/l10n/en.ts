80142df0e9d6b0181d4158cea5e82a960a3a0687
Hendrik Volschenk <righteous.trespasser@gmail.com>
2024-08-31 22:55:47 +0200

    fix(client): remove authentication message

--- client/src/l10n/en.ts
+++ client/src/l10n/en.ts
@@ -5,8 +5,6 @@ const en: L10n = {
   actionTryAgain: 'Try again',
   applicationName: 'iamcare',
   authenticateActionLogin: 'Login',
-  authenticateAlertNoRegistrationRequired:
-    'No registration required, you can log in and use {applicationName} immediately',
   authenticateGoogleActionLogin: 'Login with Google',
   authenticateGoogleErrorLoginFailed: 'There was an error while logging you in',
   authenticateProviderErrorLoading:
