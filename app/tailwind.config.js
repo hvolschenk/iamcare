e66211a47c8e7662071801ac323179fb2f2cfe84
Hendrik Volschenk <righteous.trespasser@gmail.com>
2024-09-23 23:09:24 +0200

    feat: add tailwind service
    Add `tailwind` service to compile stylesheets during runtime.
    It would have been preferred to just use the CDN for now,
    but there is no configuration to specify a nonce for the inline styles.

---
+++ app/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/css/app.css",
+      ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "hsl(15, 18%, 47%)",
+                secondary: "hsl(340, 82%, 76%)",
+            },
+            fontFamily: {
+                sans: ['Open Sans', 'sans-serif'],
+            },
+        },
+    },
+};
