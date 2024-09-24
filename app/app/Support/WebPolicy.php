a81f6b8c575b866622ebf5478645ed3efc464372
Hendrik Volschenk <righteous.trespasser@gmail.com>
2024-09-24 22:34:53 +0200

    feature: content security policy
    Add simple Content Security Policy through `spatie/laravel-csp`.

---
+++ app/app/Support/WebPolicy.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Support;
+
+use Spatie\Csp\Directive;
+use Spatie\Csp\Policies\Basic;
+
+class WebPolicy extends Basic
+{
+    public function configure()
+    {
+        parent::configure();
+
+        $this
+            ->addDirective(Directive::CONNECT, ['https://accounts.google.com/gsi/'])
+            ->addDirective(Directive::FONT, ['https://fonts.gstatic.com'])
+            ->addDirective(Directive::FRAME, ['https://accounts.google.com/gsi/'])
+            ->addDirective(Directive::SCRIPT, [
+                'https://accounts.google.com/gsi/client',
+                'https://unpkg.com/htmx.org@2.0.2',
+            ])
+            ->addDirective(Directive::STYLE, [
+                'https://accounts.google.com/gsi/style',
+                'https://fonts.googleapis.com',
+            ])
+            ->addNonceForDirective(Directive::STYLE);
+    }
+}
--- app/bootstrap/app.php
+++ app/bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(\Spatie\Csp\AddCspHeaders::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
