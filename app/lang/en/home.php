bef0d7215ae44e856434423dac06dd4d3c103d5c
Hendrik Volschenk <righteous.trespasser@gmail.com>
2024-09-24 22:50:42 +0200

    feat: update home page with hero and overview

---
+++ app/lang/en/home.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'give__action' => 'Give an item',
+    'give__description' => 'Many of us have items in our homes which we do not need. These could be treasures to others.',
+    'give__sub-heading' => 'Give away what you don\'t need',
+    'give__title' => 'Give',
+    'take__action' => 'Search for items',
+    'take__description' => 'We are all here to support each other. Simply search and send a message about any item(s) you need.',
+    'take__sub-heading' => 'Take any item(s) you need',
+    'take__title' => 'Take',
+];
