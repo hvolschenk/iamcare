6f72e7be9765570b8e3db12bb3c1b854488ec3bf
Hendrik Volschenk <righteous.trespasser@gmail.com>
2024-12-14 21:52:09 +0100

    chore: wrap search dialog in search component
    Slightly better semantically

--- app/resources/views/components/layouts/search-dialog.blade.php
+++ app/resources/views/components/layouts/search-dialog.blade.php
@@ -16,50 +16,52 @@
         w-full"
     id="search-dialog"
 >
-    <form action="{{ route('search') }}" class="p-6" method="GET">
-        <x-forms.label field="query" for="query" label="{{ __('search.field__query--label') }}" />
-        <input
-            autofocus
-            class="
-                border-gray-500
-                border
-                dark:bg-gray-700
-                focus:border-primary
-                focus:outline
-                focus:outline-primary
-                mb-2
-                mt-1
-                p-2
-                rounded
-                w-full"
-            id="query"
-            name="query"
-            type="text"
-        />
-        <x-forms.helper-text
-            field="query"
-            helperText="{{ __('search.field__query--helper-text') }}"
-        />
-
-        <div class="flex justify-end">
-            <button
+    <search>
+        <form action="{{ route('search') }}" class="p-6" method="GET">
+            <x-forms.label field="query" for="query" label="{{ __('search.field__query--label') }}" />
+            <input
+                autofocus
                 class="
-                    bg-primary
-                    dark:disabled:bg-gray-500
-                    dark:disabled:text-gray-200
-                    disabled:bg-gray-200
-                    disabled:text-gray-500
-                    hover:bg-primary/80
-                    md:w-auto
-                    px-4
-                    py-2
+                    border-gray-500
+                    border
+                    dark:bg-gray-700
+                    focus:border-primary
+                    focus:outline
+                    focus:outline-primary
+                    mb-2
+                    mt-1
+                    p-2
                     rounded
-                    text-gray-50
                     w-full"
-                type="submit"
-            >
-                {{ __('search.action__search') }}
-            </button>
-        </div>
-    </form>
+                id="query"
+                name="query"
+                type="text"
+            />
+            <x-forms.helper-text
+                field="query"
+                helperText="{{ __('search.field__query--helper-text') }}"
+            />
+
+            <div class="flex justify-end">
+                <button
+                    class="
+                        bg-primary
+                        dark:disabled:bg-gray-500
+                        dark:disabled:text-gray-200
+                        disabled:bg-gray-200
+                        disabled:text-gray-500
+                        hover:bg-primary/80
+                        md:w-auto
+                        px-4
+                        py-2
+                        rounded
+                        text-gray-50
+                        w-full"
+                    type="submit"
+                >
+                    {{ __('search.action__search') }}
+                </button>
+            </div>
+        </form>
+    </search>
 </dialog>
