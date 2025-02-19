50b7aad9e27beba51a5f24cb9711f4c2f40f5b81
Hendrik Volschenk <righteous.trespasser@gmail.com>
2025-02-19 22:45:07 +0100

    feat: home page content
    Latest _Items_ and popular _Tags_

--- app/resources/views/pages/home.blade.php
+++ app/resources/views/pages/home.blade.php
@@ -42,4 +42,62 @@
             </button>
         </div>
     </section>
+
+    @if (count($latestItems) > 0)
+        <section class="mt-6">
+            <h2 class="font-bold mb-4 text-xl">
+                {{ __('home.latest-items__title') }}
+            </h2>
+            <x-items-list :items="$latestItems" />
+        </section>
+    @endif
+
+    <section class="mt-12 text-center">
+        <h2 class="font-bold mb-4 text-xl">
+            {{ __('home.issues-suggestions__title') }}
+        </h2>
+        <p class="dark:text-neutral-300 text-neutral-500">
+            {{ __('home.issues-suggestions__description') }}
+        </p>
+        <a
+            class="gap-2 hover:bg-primary/20 inline-flex mt-3 py-1 px-2 rounded text-center text-primary"
+            href="https://github.com"
+        >
+            <span class="material-symbols-outlined">bug_report</span>
+            {{ __('home.issues-suggestions__action') }}
+        </a>
+    </section>
+
+    @if (count($popularTags) > 0)
+        <section
+            class="
+                bg-neutral-50
+                border
+                border-neutral-500
+                dark:bg-neutral-900
+                dark:hover:bg-black
+                dark:hover:border-neutral-400
+                hover:bg-white
+                hover:border-neutral-600
+                mt-12
+                p-4
+                pb-6
+                rounded"
+        >
+            <h2 class="font-bold mb-4 text-xl">
+                {{ __('home.popular-tags__title') }}
+            </h2>
+            <div class="gap-4 grid grid-cols-2 md:grid-cols-3">
+                @foreach ($popularTags as $popularTag)
+                    <p>
+                        <x-tag class="!text-sm" :tag="$popularTag">
+                            <span class="bg-neutral-300 dark:bg-neutral-900 inline-flex ml-2 px-2 py-1/2 rounded-full">
+                                {{ $popularTag->items_count }}
+                            </span>
+                        </x-tag>
+                    </p>
+                @endforeach
+            </div>
+        </section>
+    @endif
 </x-layouts.base>
