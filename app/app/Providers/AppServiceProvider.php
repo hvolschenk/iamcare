66241a504c595e8badc47bb3573cf7d8bd006250
Hendrik Volschenk <righteous.trespasser@gmail.com>
2025-03-17 22:43:17 +0100

    chore: use observer on image model
    Swap from `boot` method to _Observer_
    for reacting to events on the _Item_ model.

--- app/app/Providers/AppServiceProvider.php
+++ app/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Item;
+use App\Observers\ItemObserver;
 use App\View\Composers\AppBarComposer;
 use App\View\Composers\ItemFormComposer;
 use Illuminate\Support\Facades\Http;
@@ -27,6 +29,9 @@ class AppServiceProvider extends ServiceProvider
         Http::macro('googlePlaces', function () {
             return Http::baseUrl('https://maps.googleapis.com/maps/api');
         });
+
+        Item::observe(ItemObserver::class);
+
         Paginator::defaultView('components.pagination');
 
         View::composer('components.layouts.base.app-bar', AppBarComposer::class);
