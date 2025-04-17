95401093e6035da0c20c88a53c90d7f6946024f3
Hendrik Volschenk <righteous.trespasser@gmail.com>
2025-04-17 23:30:39 +0200

    fix: verify all emails
    * Add necessary newlines.
    * Remove double titles.
    * Remove unnecessary indentation.

--- app/resources/views/emails/account/created.blade.php
+++ app/resources/views/emails/account/created.blade.php
@@ -1,6 +1,5 @@
 <x-layouts.mail>
-{{ __('mail.account-created__title') }}
-
 {{ __('mail.greeting', ['name' => $name]) }}
+
 {{ __('mail.account-created__body') }}
 </x-layouts.mail>
--- app/resources/views/emails/item/reported.blade.php
+++ app/resources/views/emails/item/reported.blade.php
@@ -1,10 +1,11 @@
 <x-layouts.mail>
-    {{ __('mail.greeting', ['name' => 'administrator']) }}
-    {{
-        __('mail.item-reported__body', [
-            'item' => "{$itemReport->item->name} (#{$itemReport->item->id})",
-            'reason' => $itemReport->reason,
-            'user' => "{$itemReport->user->name} (#{$itemReport->user->id})",
-        ])
-    }}
+{{ __('mail.greeting', ['name' => 'administrator']) }}
+
+{{
+    __('mail.item-reported__body', [
+        'item' => "{$itemReport->item->name} (#{$itemReport->item->id})",
+        'reason' => $itemReport->reason,
+        'user' => "{$itemReport->user->name} (#{$itemReport->user->id})",
+    ])
+}}
 </x-layouts.mail>
